77b26784cf547864e641963d18cf6e587738555e
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-03-13 23:27:32 -0500

    added particle system

--- Assets/Scripts/Firewall.cs
+++ Assets/Scripts/Firewall.cs
@@ -6,11 +6,14 @@ public class Firewall : MonoBehaviour
     public float force = 100;    
 
     private Rigidbody rb;
+    private ParticleSystem part;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        part = GetComponentInChildren<ParticleSystem>();
+        part.Stop();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -18,6 +21,7 @@ public class Firewall : MonoBehaviour
         if(collision.gameObject.tag == "Player")
         {
             rb.AddForce(collision.transform.forward * force);
+            part.Play();
         }
     }
 }
